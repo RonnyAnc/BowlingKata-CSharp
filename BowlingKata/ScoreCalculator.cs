e0113987f4f58f6942dfea7b49805ba62c19ba2e
Ronny Ancorini <Ronnygx@gmail.com>
2016-03-20 01:38:05 +0000

    Refactor: extract new class which has the construction responsibility

--- BowlingKata/ScoreCalculator.cs
+++ BowlingKata/ScoreCalculator.cs
@@ -7,39 +7,20 @@ namespace BowlingKata
 	public class ScoreCalculator
 	{
 		private Frame Frame { get; set; }
+	    private FrameStructureConstructor constructor;
 
 	    public ScoreCalculator(char[] line)
 		{
-			Frame = GetFramesFrom(line);
+            constructor = new FrameStructureConstructor();
+            Frame = GetFramesFrom(line);
 		}
 
 		private Frame GetFramesFrom(char[] line)
 		{
 			var rolls = RollsFrom(line);
-		    Frame frame = ConstructFrames(rolls, 0);
+		    Frame frame = constructor.ConstructFrames(rolls, 0);
 		    return frame;
 		}
-
-	    private Frame ConstructFrames(List<Roll> rolls, int index)
-	    {
-	        if (index == rolls.Count - 2) return new Frame(rolls[index], rolls[index + 1]);
-            if (rolls[index] == Strike) return StrikeFrame(rolls, index);
-            return NormalFrame(rolls, index);
-        }
-
-	    private Frame NormalFrame(List<Roll> rolls, int index)
-	    {
-	        var frame = new Frame(rolls[index], rolls[index + 1]);
-	        frame.Next = ConstructFrames(rolls, index + 2);
-	        return frame;
-	    }
-
-	    private Frame StrikeFrame(List<Roll> rolls, int index)
-	    {
-	        var frame = new Frame(Strike, Zero);
-	        frame.Next = ConstructFrames(rolls, index + 1);
-	        return frame;
-	    }
 
 	    private List<Roll> RollsFrom(char[] line)
 		{
@@ -65,5 +46,29 @@ namespace BowlingKata
 		    return score;
 		}
 	}
+
+    internal class FrameStructureConstructor
+    {
+        public Frame ConstructFrames(List<Roll> rolls, int index)
+        {
+            if (index == rolls.Count - 2) return new Frame(rolls[index], rolls[index + 1]);
+            if (rolls[index] == Strike) return StrikeFrame(rolls, index);
+            return NormalFrame(rolls, index);
+        }
+
+        private Frame NormalFrame(List<Roll> rolls, int index)
+        {
+            var frame = new Frame(rolls[index], rolls[index + 1]);
+            frame.Next = ConstructFrames(rolls, index + 2);
+            return frame;
+        }
+
+        private Frame StrikeFrame(List<Roll> rolls, int index)
+        {
+            var frame = new Frame(Strike, Zero);
+            frame.Next = ConstructFrames(rolls, index + 1);
+            return frame;
+        }
+    }
 }
 
