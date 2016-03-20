936321d31a4608c483ec25b88dcf9086f24820db
Ronny Ancorini <Ronnygx@gmail.com>
2016-03-20 01:32:58 +0000

    Refactor: remove old code + rename

--- BowlingKata/ScoreCalculator.cs
+++ BowlingKata/ScoreCalculator.cs
@@ -16,32 +16,11 @@ namespace BowlingKata
 		private Frame GetFramesFrom(char[] line)
 		{
 			var rolls = RollsFrom(line);
-		    Frame frame = fooFrames(rolls, 0);
-		    return frame;
-		    if (rolls[0] == Strike)
-		    {
-		        frame = new Frame(Strike, Zero);
-                frame.Next = new Frame(rolls[1], rolls[2]);
-            }
-			else
-		    {
-		        frame = new Frame(rolls[0], rolls[1]);
-		        if (rolls[2] == Strike)
-		        {
-		            frame.Next = new Frame(Strike, Zero);
-		            frame.Next.Next = new Frame(rolls[3], rolls[4]);
-		        }
-		        else
-		        {
-                    frame.Next = new Frame(rolls[2], rolls[3]);
-                    frame.Next.Next = new Frame(rolls[4], rolls[5]);
-                }
-            }
-            
+		    Frame frame = ConstructFrames(rolls, 0);
 		    return frame;
 		}
 
-	    private Frame fooFrames(List<Roll> rolls, int index)
+	    private Frame ConstructFrames(List<Roll> rolls, int index)
 	    {
 	        if (index == rolls.Count - 2) return new Frame(rolls[index], rolls[index + 1]);
             if (rolls[index] == Strike) return StrikeFrame(rolls, index);
@@ -51,14 +30,14 @@ namespace BowlingKata
 	    private Frame NormalFrame(List<Roll> rolls, int index)
 	    {
 	        var frame = new Frame(rolls[index], rolls[index + 1]);
-	        frame.Next = fooFrames(rolls, index + 2);
+	        frame.Next = ConstructFrames(rolls, index + 2);
 	        return frame;
 	    }
 
 	    private Frame StrikeFrame(List<Roll> rolls, int index)
 	    {
 	        var frame = new Frame(Strike, Zero);
-	        frame.Next = fooFrames(rolls, index + 1);
+	        frame.Next = ConstructFrames(rolls, index + 1);
 	        return frame;
 	    }
 
