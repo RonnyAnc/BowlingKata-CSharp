771bf36cbf67d8082f1f000e7ec7e9665a6d3ac5
Ronny Ancorini <Ronnygx@gmail.com>
2016-03-20 00:04:58 +0000

    Remove unused properties

--- BowlingKata/Frame.cs
+++ BowlingKata/Frame.cs
@@ -2,27 +2,19 @@ namespace BowlingKata
 {
 	public class Frame
 	{
-		private int FirstRoll { get; }
-		private int SecondRoll { get; }
-		private Roll FirstRoll1 { get; }
-		private Roll SecondRoll1 { get; }
+		private Roll FirstRoll { get; }
+		private Roll SecondRoll { get; }
 
-		public Frame(int firstRoll, int secondRoll)
+		public Frame(Roll firstRoll, Roll secondRoll)
 		{
 			FirstRoll = firstRoll;
 			SecondRoll = secondRoll;
 		}
 
-		public Frame(Roll firstRoll, Roll secondRoll)
-		{
-			FirstRoll1 = firstRoll;
-			SecondRoll1 = secondRoll;
-		}
-
 		public int Score()
 		{
-			if (SecondRoll1 == Roll.Spare) return 10;
-			return (int) FirstRoll1 + (int) SecondRoll1;
+			if (SecondRoll == Roll.Spare) return 10;
+			return (int) FirstRoll + (int) SecondRoll;
 		}
 	}
 }
